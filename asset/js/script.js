9b1c25e5528852a9ba90d172d585bd4ccea45c9d
Eli-ngo <elisab.ngo@gmail.com>
2021-05-12 23:05:40 +0200

    nouvelle navbar

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+
+    burger.addEventListener('click', () => {
+        //Apparition nav
+        nav.classList.toggle('nav-active');
+
+        //Animation des liens
+        navLinks.forEach((link, indexclean) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = 'navLinkFade 0.5s ease forwards ${indexclean / 7 + 0.3}s'
+            }
+        });
+
+        // Animation du burger
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
